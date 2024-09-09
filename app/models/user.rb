069387c79c1d5d9fbea98a55ee4824edd224c960
Jessica Manturano <jessica95.mp@gmail.com>
2024-09-09 15:58:31 -0500

    validations for model users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :pets
+  belongs_to :service
+
+  validates :first_name, :last_name, :address, :phone_number, :description, presence: true
 end
