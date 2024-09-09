05bba209c469107445ff44635c629ef4ac09f0e9
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <diegoby1994@gmail.com>
2024-09-09 17:40:06 -0500

    service controller done

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,26 @@
+class ServicesController < ApplicationController
+  before_action :set_user
+
+  def index
+    @services = @user.services
+  end
+
+  def new
+    @service = @user.services.build
+  end
+
+  def create
+    @service = @user.services.build(service_params)
+    redirect_to @service, notice: 'Service created' if @service.save
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def service_params
+    params.require(:service).permit(:price, :description, :address, :latitude, :longitude, :restrictions, :house_description)
+  end
+end
