af1d9b34df9c19e48f05e3188ac70254a4418810
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <diegoby1994@gmail.com>
2024-09-09 16:31:59 -0500

    service validation done

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,13 @@
 class Service < ApplicationRecord
   belongs_to :user
+  has_many :booking
+
+  validates :price, presence: true, numericality: true
+  validates :description, presence: true, lenght: { maximum: 500 }
+  validates :address, presence: true, lenght: { maximum: 200 }
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+  validates :photo, presence: true
+  validates :restrictions, presence: true, lenght: { maximum: 200 }
+  validates :house_description, presence: true, lenght: { maximum: 500 }
 end
