5d02c440f445559c2f7d3a7b527a0d7076bda222
Daniela Mondragon <dani.mariana2901@gmail.com>
2024-09-11 12:56:11 -0600

    Created the landing route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
 
   def redirect_authenticated_user
     if user_signed_in?
-      redirect_to services_path
+      redirect_to landing_path
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
   root to: "pages#home"
   get "up" => "rails/health#show", as: :rails_health_check
   resources :users
   resources :services
+  get 'landing', to: 'services#landing'
 
   # Defines the root path route ("/")
   # root "posts#index"
