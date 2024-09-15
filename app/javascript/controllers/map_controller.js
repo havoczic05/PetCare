f40a6190bafae82f1b08bdf54bcb4ffeb98784f2
Jessica Manturano <jessica95.mp@gmail.com>
2024-09-15 00:31:13 -0500

    show map in service detail

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,7 +1,34 @@
 import { Controller } from "@hotwired/stimulus"
-
-// Connects to data-controller="map"
+import mapboxgl from 'mapbox-gl'
 export default class extends Controller {
+  static values = { marker: String, apiKey: String }
+
   connect() {
+    mapboxgl.accessToken = this.apiKeyValue;
+    mapboxgl.accessToken = this.apiKeyValue
+
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/mapbox/navigation-night-v1"
+    })
+
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
+  }
+
+  #addMarkersToMap () {
+    console.log({m: this.markerValue})
+    this.marker = JSON.parse(this.markerValue)
+    const popup = new mapboxgl.Popup().setHTML(this.marker.info_window_html)
+    new mapboxgl.Marker()
+      .setLngLat([ this.marker.lng, this.marker.lat ])
+      .setPopup(popup)
+      .addTo(this.map)
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    bounds.extend([this.marker.lng, this.marker.lat])
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 }
