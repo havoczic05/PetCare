35fe10f18a1fcc0cec3cd7ba7cbdaea2b9e04f05
Daniela Mondragon <dani.mariana2901@gmail.com>
2024-09-16 15:17:07 -0600

    Activities front

--- app/models/booking_calendar.rb
+++
@@ -1,9 +0,0 @@
-class SimpleCalendar::BookingCalendar < SimpleCalendar::Calendar
-  private
-
-  def date_range
-    beginning = @booking.start_date
-    ending = @booking.end_date
-    (beginning..ending).to_a
-  end
-end
