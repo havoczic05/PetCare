e2b49f6aea98908ecd127fad6fff13f1b58fed6c
Daniela Mondragon <dani.mariana2901@gmail.com>
2024-09-17 13:19:09 -0600

    Fix de log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'sessions' }
   root to: "pages#home"
   get "up" => "rails/health#show", as: :rails_health_check
   resources :users
